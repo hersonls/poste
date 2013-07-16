342e9d70f58ffe18bb75c1819b5b402f11f3fb4b
Hersonls <hersonls@gmail.com>
2013-07-15 21:46:18 -0300

    Fixed DetailView url

--- src/poste/views.py
+++ src/poste/views.py
@@ -1,11 +1,21 @@
+from django.views.generic import DetailView
 from django.views.generic import DateDetailView
+from django.views.generic.base import TemplateResponseMixin
 
 
-class PostDateDetailView(DateDetailView):
+class BaseTemplateMixin(TemplateResponseMixin):
     def get_template_names(self):
-        template_list = super(PostDateDetailView, self).get_template_names()
+        template_list = super(BaseTemplateMixin, self).get_template_names()
 
         # Adding default post detail template
         template_list.append('poste/post_detail.html')
 
         return template_list
+
+
+class PostDetailView(BaseTemplateMixin, DetailView):
+    pass
+
+
+class PostDateDetailView(BaseTemplateMixin, DateDetailView):
+    pass
